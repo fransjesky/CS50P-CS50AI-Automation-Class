7b5ecbc8f30813b1564762af5364c2a340e3bab0
jesky <jesky.yeremi@gmail.com>
2024-11-20 01:29:45 +0700

    correcting automation task-1.py

--- automation/task-1.py
+++ automation/task-1.py
@@ -6,11 +6,12 @@
 # "My name is Mike, and I'm a Programmer. My monthly net income is $12,200"
 
 def main():
-  name = input("What's your name? ")
-  job = input("What's your job? ")
-  salary = int(input("How much is your monthly income? "))
-  netWorth = salary - (salary * 5 / 100)
+  name: str = input("What's your name? ")
+  job: str = input("What's your job? ")
+  salary: int = int(input("How much is your monthly income? "))
+  netWorth: int = salary - (salary * 5 / 100)
 
-  print(f"My name is {name.capitalize()}, and I'm a {job.title()}. My monthly net income is ${netWorth:,}.")
+  print(f"My name is {name.title()}, and I'm a {job.title()}. My monthly net income is ${netWorth:,}.")
+
 
 main()
